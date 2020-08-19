737e88a962603e020272a87bdbc36756707dea9c
19031193-Quek-Bryent <19031193@myrp.edu.sg>
2020-08-19 14:04:38 +0800

    Update 2

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -10,16 +10,12 @@ public class C206_CaseStudy {
 		switch (option) {
 		case 1: //Member 1 (Bryent)
 			UserDB.showUserMenu();
-			break;
 		case 2: //Member 2 (KaiQi)
 			Item.menu();
-			break;
 		case 3: //Member 3 (Hykal)
 			
-			break;
 		case 4: //Member 4 (TengYik)
 			
-			break;
 		}
 		
 	}	
