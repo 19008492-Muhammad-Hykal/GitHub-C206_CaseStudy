5cf1a565c09b6f425cefc4940e80c144e7cdd335
19031193-Quek-Bryent <19031193@myrp.edu.sg>
2020-08-19 09:44:28 +0800

    Menu for user to access methods and also internal methods for user_account obj

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,40 @@
 
 public class C206_CaseStudy {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		menu();
+		int option = Helper.readInt("Enter Option > ");
+		switch (option) {
+		case 1: //Member 1 (Bryent)
+			accountMenu();
+			int optionAccount = Helper.readInt("Enter Option > ");
+			
+			
+		}
 	}
+	
+	public static void menu() {
+		Helper.line(40, "=");
+		System.out.println("Campus Online Auction Shop (COAS)");
+		System.out.println("1. User account");
+		System.out.println("2. Item");
+		System.out.println("3. Bid");
+		System.out.println("4. Deal");
+		System.out.println("5. Category ");
+		
+	}
+	
+	//Member 1 (Bryent)
+	public static void accountMenu() {
+		Helper.line(40, "-");
+		System.out.println("User Account Management");
+		System.out.println("1. Add user account");
+		System.out.println("2. View all users");
+		System.out.println("3. Delete user based on email");
+	}
+	
+
 
 }
