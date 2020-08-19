aae8996af4de57538668f22d388d14d867aca6cd
19008492 <19008492@DESKTOP-2K03JPT>
2020-08-19 15:21:59 +0800

    Updated_BidMethod_And_bid_class

--- C206_CaseStudy/src/bid.java
+++ C206_CaseStudy/src/bid.java
@@ -3,27 +3,21 @@
 public class bid {
 	
 	private int bid_id;
-	private int buyer_id;
-	private float bid_price;
+	private double bid_price;
 	private String item_name;
-	private String buyer_name;
-	private String seller_name;
 	private String seller_email;
 	private String buyer_email;
 	
-	public bid(int bid_id, int buyer_id, float bid_price, String item_name, String buyer_name, String seller_name, String seller_email, String buyer_email) {
+	public bid(int bid_id, double bid_price, String item_name, String seller_email, String buyer_email) {
 
 		this.bid_id = bid_id;
-		this.buyer_id = buyer_id;
 		this.bid_price = bid_price;
 		this.item_name = item_name;
-		this.buyer_name = buyer_name;
-		this.seller_name = seller_name;
 		this.seller_email = seller_email;
 		this.buyer_email = buyer_email;
 	}
 
-	public float getBid_price() {
+	public double getBid_price() {
 		return bid_price;
 	}
 
@@ -41,18 +35,6 @@ public class bid {
 
 	public int getBid_id() {
 		return bid_id;
-	}
-
-	public int getBuyer_id() {
-		return buyer_id;
-	}
-
-	public String getBuyer_name() {
-		return buyer_name;
-	}
-
-	public String getSeller_name() {
-		return seller_name;
 	}
 
 	public String getSeller_email() {
---
+++ C206_CaseStudy/src/bidMethod.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+
+public class bidMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<bid> bidList = new ArrayList<bid>();
+		
+		
+		int option = 0;
+
+		while (option != 5) {
+			bidMethod.menu(); 
+			option = Helper.readInt("Enter an option > ");
+			
+			
+		}
+
+		
+	}
+	
+	public static void menu() {
+		bidMethod.setHeader("BID SECTION");
+		System.out.println("1. Show Bid");
+		System.out.println("2. Add Bid");
+		System.out.println("3. Delete Bid");
+		System.out.println("4. Update Bid"); //New Requirement
+		System.out.println("5. Search Bid by name"); //New requirement
+		System.out.println("6. Quit");
+		Helper.line(80, "-");
+
+	}
+
+	private static void setHeader(String header) {
+		// TODO Auto-generated method stub
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+		
+	}
+	
+	public static String retrieveAllBidding(ArrayList<bid> bidList) {
+		String output = "";
+
+		for (int i = 0; i < bidList.size(); i++) {
+
+			output += String.format("%-10s %-30d %-10f %-10s %-20s\n", bidList.get(i).getItem_name(),bidList.get(i).getBid_id(),bidList.get(i).getBid_price(),
+					bidList.get(i).getBuyer_email(),bidList.get(i).getSeller_email());
+				
+		}
+		return output;
+	}
+	
+	public static void viewAllBidding(ArrayList<bid> bidList) {
+		bidMethod.setHeader("BID LIST");
+		String output = String.format("%-10s %-30s %-10s %-10s %-20s\n", "ITEM NAME", "BID ID",
+				"BID PRICE", "BUYER EMAIL","SELLER EMAIL");
+		 output += retrieveAllBidding(bidList);	
+		System.out.println(output);
+	}
+	
+	
+	public static bid inputBid() {
+		
+		int id = Helper.readInt("Enter Bid ID > ");
+		double price = Helper.readFloat("Enter price > ");
+		String item_name = Helper.readString("Enter item name > ");
+		String seller_email = Helper.readString("Enter a seller email > ");
+		String buyer_email = Helper.readString("Enter buyer email address > ");
+	
+		bid addBid = new bid(id,price,item_name,seller_email,buyer_email);
+		return addBid;
+	}
+	
+	public static void addBid(ArrayList<bid> bidList, bid addBid) {
+		
+		bidList.add(addBid);
+		System.out.println("Bid added");
+	}
+	
+	public static void deleteBid(ArrayList<bid> bidList) {
+		int deleteBidID = Helper.readInt("Enter ID of Bid to Delete > ");
+		for(int i =0; i<bidList.size();i++) {
+			if(deleteBidID == bidList.get(i).getBid_id()) {
+				bidList.remove(i);
+			}
+			else {
+				System.out.println("Invalid ID to, Delete Failed");
+			}
+		}
+	}
+
+}
+
+
+
+
+
