692034b6f10aeafdb0fc16d1da530cac196312c2
19047027 <19047027@DESKTOP-HTRBJHT>
2020-08-19 09:55:57 +0800

    add item class

--- C206_CaseStudy/src/Item.java
+++ C206_CaseStudy/src/Item.java
@@ -1,4 +1,22 @@
+import java.util.Date;
 
 public class Item {
+	public String name;
+	public String description;
+	public double bid_price;
+	public Date start_date;
+	public Date end_date;
+	
+	public Item(String name, String description, double bid_price, Date start_date, Date end_date) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.bid_price = bid_price;
+		this.start_date = start_date;
+		this.end_date = end_date;
+	}
+	
+	
+	
 
 }
