c8059f68ca8c8c28be0b1937c0fa593ac62fd921
19047027 <19047027@DESKTOP-HTRBJHT>
2020-08-19 11:49:58 +0800

    item change

--- C206_CaseStudy/src/Item.java
+++ C206_CaseStudy/src/Item.java
@@ -7,6 +7,8 @@ public class Item {
 	public String start_date;
 	public String end_date;
 	
+	ArrayList<Item> itemList = new ArrayList<Item>();
+	itemList.add(new Item("Bear", "Cute and Soft", 3.50,"16/4/2020","20/4/2020"));
 	public Item(String name, String description, double bid_price, String start_date, String end_date) {
 		super();
 		this.name = name;
@@ -15,8 +17,6 @@ public class Item {
 		this.start_date = start_date;
 		this.end_date = end_date;
 		
-		ArrayList<Item> itemList = new ArrayList<Item>();
-		itemList.add(new Item("Bear", "Cute and Soft", 3.50,"16/4/2020","20/4/2020"));
 		
 		
 	}
