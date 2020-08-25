7a953601f545b46cf13f8af10f0ddc6ff185cdac
19008492 <19008492@DESKTOP-2K03JPT>
2020-08-25 18:43:33 +0800

    Update bidMethodTest

--- C206_CaseStudy/test/bidMethodTest.java
+++ C206_CaseStudy/test/bidMethodTest.java
@@ -28,7 +28,7 @@ public class bidMethodTest {
 	@Test
 	
 	public void addBidTest() {
-		// Item list is not null, so that can add a new item - Boundary Test
+		// Bid list is not null, so that can add a new item - Boundary Test
 		assertNotNull("Test if there is valid Bid arraylist to add to", bidList);
 		
 		//Given an empty list, after adding 1 item, the size of the list is 1 - Normal Test
@@ -41,7 +41,7 @@ public class bidMethodTest {
 	}
 	
 	public void retrieveAllBidTest() {
-		// Test if Item list is not null but empty, so that can add a new item
+		// Test if bid list is not null but empty, so that can add a new item
 		assertNotNull("Test if there is valid Bid arraylist to add to", bidList);
 		
 		//test if the list of Bid retrieved from the bidMethod is empty
@@ -49,22 +49,22 @@ public class bidMethodTest {
 				String testOutput = "";
 				assertEquals("Check that ViewAllBidlist", testOutput, allBid);
 				
-		//Given an empty list, after adding 2 items, test if the size of the list is 2
+		//Given an empty list, after adding 1 items, test if the size of the list is 1
 		bidMethod.addBid(bidList, b1);
 	
-		assertEquals("Test if that Camcorder arraylist size is 1?", 1, bidList.size());
+		assertEquals("Test if that Bid arraylist size is 1?", 1, bidList.size());
 		
 		//test if the expected output string same as the list of Bids retrieved 
 		allBid= bidMethod.retrieveAllBidding(bidList); 
 
 		testOutput = String.format("%-10d %-30f %-10s %-10s %-20s\n",2,40,"KEYBOARD","seller@mail.com","buyer@mail.com");
 		
-		assertEquals("Check that ViewAllCamcorderlist", testOutput, allBid);
+		assertEquals("Check that ViewAllBidlist", testOutput, allBid);
 		
 	}
 	
 	public void deleteBidTest() {
-		// Test if Item list is not null but empty, so that can add a new item
+		// Test if bid list is not null but empty, so that can add a new item
 		assertNotNull("Test if there is valid Bid arraylist to add to", bidList);
 		
 		//Given an empty list, after adding 1 Bid, the size of the bidList is 1 - normal
@@ -81,23 +81,23 @@ public class bidMethodTest {
 	
 	@Test
 	public void searchBid() {
-		// Item list is not null, so that can add a bid - boundary
-		assertNotNull("Check if there is valid user arraylist to add to", bidList);
+		// bid list is not null, so that can add a bid - boundary
+		assertNotNull("Check if there is valid bid arraylist to add to", bidList);
 		// Given an empty list, after adding 1 bid, the size of the bidlist is 1 -
 		// normal
 		bidMethod.addBid(bidList,b1);
-		assertEquals("Check that user arraylist size is 1", 1, bidList.size());
-		assertSame("Check that user is added", b1, bidList.get(0));
-		// Search user based on item Name and ensure user exists by comparison
+		assertEquals("Check that bid arraylist size is 1", 1, bidList.size());
+		assertSame("Check that bid is added", b1, bidList.get(0));
+		// Search bid based on item Name and ensure bid exists by comparison
 		String Realoutput = bidMethod.searchByItemName(bidList);
 		String output = bidList.get(0).toString();
-		assertEquals("Check that search returns correct user", Realoutput, output);
+		assertEquals("Check that search returns correct bid", Realoutput, output);
 	}
 
 	
 	public void updateAccount() {
-		// Bid list is not null, so that can add a new user - boundary
-		assertNotNull("Check if there is valid user arraylist to add to", bidList);
+		// Bid list is not null, so that can add a new bid - boundary
+		assertNotNull("Check if there is valid bid arraylist to add to", bidList);
 		// Given an empty list, after adding 1 Bid, the size of the bidlist is 1 -
 		// normal
 		bidMethod.addBid(bidList,b1);
